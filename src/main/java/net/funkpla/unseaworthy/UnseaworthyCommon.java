0733972d6415455a6dbf6cfd7090dfae21bc3410
Kevin Murphy <kevin@eorbit.net>
2024-06-27 06:21:17 -0700

    I made the boat sink.

--- src/main/java/net/funkpla/unseaworthy/UnseaworthyCommon.java
+++ src/main/java/net/funkpla/unseaworthy/UnseaworthyCommon.java
@@ -1,22 +1,31 @@
 package net.funkpla.unseaworthy;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.biome.Biome;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class UnseaworthyCommon implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+
     public static final Logger LOGGER = LoggerFactory.getLogger("unseaworthy");
+    public static final String MOD_ID = "unseaworthy";
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    public static TagKey<Biome> OCEANS = TagKey.create(Registries.BIOME,
+            new ResourceLocation("minecraft", "is_ocean"));
 
-		LOGGER.info("Hello Fabric world!");
-	}
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        LOGGER.info("Hello Fabric world!");
+    }
 }

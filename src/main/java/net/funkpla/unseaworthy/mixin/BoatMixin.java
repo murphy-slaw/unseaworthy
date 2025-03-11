6d8e9d3a14c55e588424fd47cb45f44ee9254a0a
Kevin Murphy <kevin@eorbit.net>
2025-03-11 13:43:34 -0700

    Add missing annotation.

--- src/main/java/net/funkpla/unseaworthy/mixin/BoatMixin.java
+++ src/main/java/net/funkpla/unseaworthy/mixin/BoatMixin.java
@@ -158,6 +158,7 @@ public abstract class BoatMixin extends VehicleEntity {
         return this.getWaterLevelBelow() >= config.minDepth && this.level().getBiome(blockPosition()).is(UnseaworthyCommon.SINKS_BOATS) && weatherBadEnough();
     }
 
+    @Unique
     boolean weatherBadEnough() {
         switch (config.weatherRequired) {
             case CLEAR -> {
